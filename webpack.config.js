0ae100ab886b5fa01c60f0897aa80deadf1d0093
Olushi Luqman <olushilukmon03@gmail.com>
2018-12-24 18:05:25 +0100

    chore(reactSetup): configure react with webpack 4 and babel
    - transpile ES6 using babel
    - configure babel with react preset
    - setup webpack-dev-server with hot reloading
    
    [Finishes #162827325]

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ],
+  devServer: {
+    clientLogLevel: 'none'
+  }
+};
